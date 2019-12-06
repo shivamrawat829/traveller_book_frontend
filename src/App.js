e78f2904e8efe814bc48fb963cd7564a9ce05fe0
shivamrawat829 <shivamrawat829@gmail.com>
2019-12-06 23:56:27 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -3,17 +3,31 @@ import './App.css';
 import Home from './container/Home'
 import {
   BrowserRouter as Router,
-  Route 
+  Route ,
+  Switch, Redirect
 } from "react-router-dom";
 import AdventureDetail from './container/AdventureDetail/AdventureDetail'
 // import PageNotFound from './components/PagNotFound';
-
+// styles for this kit
+import "./assets/css/bootstrap.min.css";
+import "./assets/scss/now-ui-kit.scss";
+import "./assets/demo/demo.css";
+import "./assets/demo/nucleo-icons-page-styles.css";
 
 // newly added
 import { connect } from 'react-redux';
 //import BaseRouter from './routes';push
 import * as actions from './store/actions/auth';
 import Profile from './container/Profile/Profile';
+import ProfilePage from './container/ProfilePage/ProfilePage';
+
+// pages for this kit
+import Index from "./views/Index.js";
+import NucleoIcons from "./views/NucleoIcons.js";
+import LoginPage from "./views/examples/LoginPage.js";
+import LandingPage from "./views/examples/LandingPage.js";
+
+
 
 class App extends React.Component {
 
@@ -29,8 +43,24 @@ render(){
       <Route path='/' exact strict component={Home}{...this.props}></Route>
       <Route path='/adventure_detail' exact strict component={AdventureDetail}></Route>
       <Route path='/profile' exact strict component={Profile}></Route>
+      <Route path='/profilepage' exact strict component={ProfilePage}></Route>
       {/* <Route component={PageNotFound}/> */}
       {/* <Home></Home> */}
+
+
+      <Route path="/index" render={props => <Index {...props} />} />
+        <Route
+          path="/nucleo-icons"
+          render={props => <NucleoIcons {...props} />}
+        />
+        <Route
+          path="/landing-page"
+          render={props => <LandingPage {...props} />}
+        />
+        <Route path="/login-page" render={props => <LoginPage {...props} />} />
+        <Redirect to="/index" />
+        <Redirect from="/" to="/index" />
+
     </div>
     </Router>
   );
